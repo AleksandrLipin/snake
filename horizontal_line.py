ee652abf29da2b814912fef469a975d3433f6392
AleksandrLipin <support_lav@tlvx.ru>
2018-03-07 14:50:21 +0900

    fix some syntax errors

--- horizontal_line.py
+++ horizontal_line.py
@@ -1,16 +1,15 @@
 from point import Point
 
 class HorizontalLine(Point):
-    def __init__(self,x_left,x_right,y,sym):
-        # self.point_list = point_list
+    def __init__( self, x_left, x_right, y, sym ):
         self.x_left = x_left
         self.x_right = x_right
         self.y = y
         self.sym = sym
 
         self.point_list = []
-        for x in range(self.x_left,self.x_right):
-            p = Point(x,self.y,self.sym)
+        for x in range( self.x_left, self.x_right + 1 ):
+            p = Point( x, self.y, self.sym )
             self.point_list.append(p)
 
     def draw(self):
