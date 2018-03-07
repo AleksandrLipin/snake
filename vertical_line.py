e8c8d77e93d8cfe5c82b69bc118e065af830b9c3
AleksandrLipin <support_lav@tlvx.ru>
2018-03-07 14:34:33 +0900

    add class VerticalLine

---
+++ vertical_line.py
@@ -0,0 +1,17 @@
+from point import Point
+
+class VerticalLine(Point):
+    def __init__( self, x, y_up, y_down, sym ):
+        self.x = x
+        self.y_up = y_up
+        self.y_down = y_down
+        self.sym = sym
+
+        self.point_list = []
+        for y in range( self.y_up, self.self.y_down ):
+            p = Point( self.x, y, self.sym )
+            self.point_list.append(p)
+
+    def draw(self):
+        for p in self.point_list:
+            print p.draw()
